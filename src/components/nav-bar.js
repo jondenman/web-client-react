d084dd810c8577c4145f09dfcf012a145ceb97fb
jdenman <denmanjonathan@gmail.com>
2021-09-08 09:10:13 -0700

    Added col breakpoints to nav-bar to make it more mobile friendly

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -4,7 +4,7 @@ import './nav-bar.css';
 
 const NavBar = () =>
     <div className="row sticky-top jd-sticky-nav-bar">
-        <ul className="nav nav-pills col-12">
+        <ul className="nav nav-pills col-1 col-sm-12">
             <div className = "col-1">
             </div>
             <li className="nav-item">
