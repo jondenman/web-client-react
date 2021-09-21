1cdf8b088b8ccb28d511eccf74accb6a48847a33
jdenman <denmanjonathan@gmail.com>
2021-09-20 18:08:46 -0700

    made nav-bar and and home more mobile friendly

--- src/components/home.js
+++ src/components/home.js
@@ -5,8 +5,9 @@ import ProjectCard from "./project-card";
 
 const Home = () =>
     <div className={"fill-window flex"} id={'home'}>
-        <h1 className={"justify-content-lg-center"}>Oh hey! I'm <strong>Jon Denman</strong>, a software developer</h1>
-        <h1> with a background in music.</h1>
+        <div className={"d-flex justify-content-lg-center p-5 p-sm-0"}>
+            <h1>Oh hey! I'm <strong>Jon Denman</strong>, <br/> a software developer with a background in music.</h1>
+        </div>
         <button className={"button"} onClick={() => {
             const anchor = document.querySelector('#about')
             anchor.scrollIntoView({ behavior: 'smooth', block: 'start' })
