69d7e7254a04fb61ecfc5dc53762e4edf13b5de3
Jon Denman <denmanjonathan@gmail.com>
2024-04-24 15:15:46 -0700

    Updates

--- src/components/journal_mine.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// import html from '../R-html/journal_data_mine.html';
-const JournalMine = () =>
-    <div></div>
-    // <div dangerouslySetInnerHTML={{ __html: html}}></div>
-
-export default JournalMine
--- src/components/music.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Music = () =>
-    <div>
-        <h1>Music</h1>
-    </div>
-
-export default Music
