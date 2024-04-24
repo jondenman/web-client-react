39c3e1063547e7861a6d486bf06d7f6bdf062433
Jon Denman <denmanjonathan@gmail.com>
2024-04-24 15:33:15 -0700

    Updates

---
+++ precache-manifest.3ff99265762236d1dcd00eaf42c9e6cb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6f2786184218ea14f904acc31a8b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "843e051f275391445207",
+    "url": "/static/css/2.9b03b5f0.chunk.css"
+  },
+  {
+    "revision": "fa98c325e69e09182b91",
+    "url": "/static/css/main.c6b3c77b.chunk.css"
+  },
+  {
+    "revision": "843e051f275391445207",
+    "url": "/static/js/2.e520c9df.chunk.js"
+  },
+  {
+    "revision": "f056774035ce39e6238293d1e83850f9",
+    "url": "/static/js/2.e520c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9249d254639cbe536aa6",
+    "url": "/static/js/3.a39a06bf.chunk.js"
+  },
+  {
+    "revision": "fa98c325e69e09182b91",
+    "url": "/static/js/main.401754de.chunk.js"
+  },
+  {
+    "revision": "b9ba36d63b5b88b5b53f",
+    "url": "/static/js/runtime-main.d80abe64.js"
+  },
+  {
+    "revision": "0e71bbfdf69e76fbe323a74211e6ee73",
+    "url": "/static/media/all-ears-homepage.0e71bbfd.png"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "fa03e08d8fe189b372bdd367a258fb87",
+    "url": "/static/media/home_bckgrnd.fa03e08d.svg"
+  },
+  {
+    "revision": "cf041bc8f052c125e4a3824a9bc836e5",
+    "url": "/static/media/journal_data_mine.cf041bc8.pdf"
+  },
+  {
+    "revision": "221db0f69b1bb9c4a097f37c1ceb01c7",
+    "url": "/static/media/linderpix-NEU-5752-web copy.221db0f6.jpg"
+  },
+  {
+    "revision": "b6f07398140f872d52ce0e6acb6a339c",
+    "url": "/static/media/particle.b6f07398.png"
+  },
+  {
+    "revision": "9b6370ce99edef747464296f52c591cb",
+    "url": "/static/media/planet_vid.9b6370ce.mov"
+  },
+  {
+    "revision": "588e0d9d7cf058acd2504b8153002ab0",
+    "url": "/static/media/serverless_diagram.588e0d9d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-client-react/precache-manifest.f12e18e097cc6c11f7c62a250a0099d0.js"
+  "/precache-manifest.3ff99265762236d1dcd00eaf42c9e6cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-client-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
